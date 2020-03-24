c0666d08a1b4b68e1e2c518b30a0b4c0e7168371
Surakarn Samkaew <tonkla@gmail.com>
2020-03-24 21:20:16 +0700

    Add find-category endpoint

--- api/src/typings/product.ts
+++ api/src/typings/product.ts
@@ -1,10 +1,20 @@
+export type ProductType = 'goods' | 'service'
+export type ProductChargeType = 'hourly' | 'daily' | 'monthly'
+
 export interface Product {
   id: string
+  type: ProductType
   name: string
+  description: string
   categoryId: string
   shopId: string
   pageId: string
   ownerId: string
   isActive: boolean
-  quantity: number
+  price: number
+  quantity?: number
+  charge?: ProductChargeType
+  openAt?: string
+  closeAt?: string
+  images?: string[]
 }
